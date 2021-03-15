de66186030a90f76e4fab579eb52bdb671adae19
Muhammad Hasan Alasady <muhmdhsn313@gmail.com>
2021-03-15 21:22:11 +0300

    Initial flutter project

---
+++ README.md
@@ -0,0 +1,6 @@
+# DSCs Iraq Dashboard Platform 
+
+
+# Contributors
+
+This platform dashboard developed by Developer Students Club of University of Babylon.
---
+++ android/app/src/main/kotlin/club/iqds/platform/frontend/dsc_iraq_platform/MainActivity.kt
@@ -0,0 +1,6 @@
+package club.iqds.platform.frontend.dsc_iraq_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
